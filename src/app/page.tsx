e64f445a4aa5b625eeaa10dd23fa7e3ad0da9565
mominovDev <mardonmominov6@gmail.com>
2024-09-08 00:10:09 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+const sayHello = (csd: string) => {
+  return (
+    <>
+    {csd}
+    </>
+  )
+}
+
+export default function Home() {
+
+
+  return (
+    <>
+    {sayHello('sss')}
+    </>
+  );
+}
